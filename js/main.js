46649193e181ba15db4f884046494fbeefc2371e
dummajor <dummajor@gmail.com>
2023-10-30 12:13:28 +0100

    update on files

--- js/main.js
+++ js/main.js
@@ -6,6 +6,16 @@
 // })
 
 
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
 
 
 // -------------- Testimonial Slideshow ------------------------------
