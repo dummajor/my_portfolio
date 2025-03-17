15a6a118d08b48158ef6896efd6034988f31eb53
Chukwudumbia Anih <dummajor@gmail.com>
2025-03-17 15:50:30 +0100

    update on the web files

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@
 
 
 
-
-
 // ----------- For Mobile Side Navigation -------------------
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -117,22 +115,22 @@ startAutoSlide();
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 50px from the top of the document, show the button
-window.onscroll = function() {
+window.onscroll = function () {
     scrollFunction()
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
 }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 // ----xxx------ Scroll to Top Button  --xxx---------------
